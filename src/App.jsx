9c82adc640ffce3f2e0e03fc12e29ad70e10c080
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-01-16 17:47:34 +0100

    feat MainHeader.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import MainHeader from "./components/MainHeader"
 
 function App() {
   
   return (
     <>
-      <h1>ciao sono la tua prima app react</h1>
+      
+      <MainHeader />
     </>
   )
 }
---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,48 @@
+const MainHeader = () => {
+    return (
+
+        <header>
+            <figure>
+                <img src="src/assets/img/dc-logo.png" alt="logo" />
+            </figure>
+
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">CHARACTERS</a>
+                    </li>
+                    <li>
+                        <a href="#">COMICS</a>
+                    </li>
+                    <li>
+                        <a href="#">MOVIES</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">GAMES</a>
+                    </li>
+                    <li>
+                        <a href="#">COLLECTIBLES</a>
+                    </li>
+                    <li>
+                        <a href="#">VIDEOS</a>
+                    </li>
+                    <li>
+                        <a href="#">FANS</a>
+                    </li>
+                    <li>
+                        <a href="#">NEWS</a>
+                    </li>
+                    <li>
+                        <a href="#">SHOP</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+    )
+}
+
+export default MainHeader
